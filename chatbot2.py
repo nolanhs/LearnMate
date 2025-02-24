1d5938da5cdb817e11b504451b3ac4b220961c60
Nolan Schiek <nschiek1@gmail.com>
2025-02-24 23:39:50 +0100

    Added chatbot using gpt-4o 1

--- chatbot2.py
+++ chatbot2.py
@@ -1,12 +1,12 @@
 import openai
 
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key="KEY HERE")
 
 def chatbot(prompt):
     response = client.chat.completions.create(
         model = "gpt-4o",
         messages = [
-            {"role": "system", "content": "You are an AI tutor called LearnMate, you are specialized in Math, Science, Language and History. Provide clear, accurate, and educational responses."},
+            {"role": "system", "content": "You are an AI tutor called LearnMate, you are specialized in Math, Business, Computer Science and Design. Provide clear, accurate, and educational responses."},
             {"role": "user", "content": prompt}
         ]
     )
